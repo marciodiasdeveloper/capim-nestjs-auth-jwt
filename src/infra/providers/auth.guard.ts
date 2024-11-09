942b70167f5d7ea24f13b5879bbbda52d1f57165
marciodias <marciodias@pop-os.localdomain>
2024-11-09 18:13:08 -0300

    feat: add files

--- src/infra/providers/auth.guard.ts
+++ src/infra/providers/auth.guard.ts
@@ -1,37 +1,37 @@
 import {
-  Injectable,
   CanActivate,
   ExecutionContext,
+  Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
+
   async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
-    if (!token) {
-      throw new UnauthorizedException();
-    }
+    if (!token) throw new UnauthorizedException();
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: process.env.JWT_SECRET,
+        secret: 'NEST_AUTH',
       });
-      console.log(payload);
+
+      request['user'] = payload;
+      return true;
     } catch {
       throw new UnauthorizedException();
     }
-
-    return true;
   }
 
+  // Bearer  dfsd8u8238shjdfsdfhsdf
   private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization.split(' ') ?? [];
-
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
 }
---
+++ src/infra/providers/roles-guard.provider.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { ROLES_KEY, Role } from '../../decorators/roles.decorators';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+
+    const { user } = request.user;
+
+    return requiredRoles.some((role) => user.roles.includes(role));
+  }
+}
